f6bccab00396ee92f96b61c813d51e6f537863c0
DejanDraschkow <dejan.draschkow@gmail.com>
2017-07-29 17:32:49 +0200

    take out START variable in experimental output

--- R/eyer_import_behavioral_data.R
+++ R/eyer_import_behavioral_data.R
@@ -1,21 +1,22 @@
 EyerImportBehavioralData <- function(raw.data.path){
-  
+
   # Documentation here
   #
   # Args:
   #
-  # Returns: 
-  
+  # Returns:
+
   file.list <- list.files(raw.data.path , pattern = "csv", recursive = F)
-  
+
   behav_dat<- NULL
   for(file in file.list){
     jj <- read.table(paste(raw.data.path, file, sep = ""),sep = ",", header = T)
     fileName <- gsub(".csv", "", file)
     jj$Participant <- factor(fileName)
+    jj$start <- NULL
     behav_dat <- bind_rows(behav_dat, jj)
-  
-    # Providing some feedback on processing progress.   
+
+    # Providing some feedback on processing progress.
     print(paste("The file:", file, "has been imported successfully.", sep = " "))
   }
   writeLines("All available files were imported and row-bound.\n Columns are matched by name, and any values that don't match will be filled with NA.")
